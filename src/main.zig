d1ca93fe251e50f36dd66a16e0592135a72593aa
Jakob <jakob.dekleva@gmail.com>
2025-03-17 19:54:12 +0100

    Feat: add the main file to git

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const print = std.debug.print;
 const bitboard = @import("bitboard.zig");
 const types = @import("types.zig");
 const util = @import("util.zig");
-
+const table_attacks = @import("table_attacks.zig");
 pub fn main() !void {
     print("Hello NeroSpeed.\n", .{});
     var bitboard_1: u64 = 0;
@@ -15,4 +15,23 @@ pub fn main() !void {
 
     print("bit on bitbord {d}\n", .{bitboard_1});
     try bitboard.print_board(bitboard_1);
+
+    var stdout = std.io.getStdOut().writer();
+    try stdout.print("White Pawn Attacks:\n", .{});
+    for (0..types.number_of_squares) |i| {
+        const sq: types.square = @enumFromInt(i);
+        var bb: types.Bitboard = 0;
+        bb = util.set_bit(bb, sq);
+        const white_attacks = table_attacks.pawn_attacks_from_bitboard(types.Color.White, bb);
+        try stdout.print("Square {d}: 0x{x}\n", .{ i, white_attacks });
+    }
+
+    try stdout.print("\nBlack Pawn Attacks:\n", .{});
+    for (0..types.number_of_squares) |i| {
+        const sq: types.square = @enumFromInt(i);
+        var bb: types.Bitboard = 0;
+        bb = util.set_bit(bb, sq);
+        const black_attacks = table_attacks.pawn_attacks_from_bitboard(types.Color.Black, bb);
+        try stdout.print("Square {d}: 0x{x}\n", .{ i, black_attacks });
+    }
 }
