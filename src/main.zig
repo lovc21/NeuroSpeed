c07106c70d6481d963bd13458edf0b161b2c1a34
Jakob <jakob.dekleva@gmail.com>
2025-05-06 21:34:34 +0200

    Feat: add move generation for pawns

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,8 @@ const bitboard = @import("bitboard.zig");
 const types = @import("types.zig");
 const util = @import("util.zig");
 const attacks = @import("attacks.zig");
+const lists = @import("lists.zig");
+const move_gen = @import("move_generation.zig");
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
@@ -16,6 +18,26 @@ pub fn main() !void {
     print("Occupancy (hex): 0x{x}\n", .{bb});
 
     b.side = types.Color.White;
+    bitboard.print_unicode_board(b);
+
+    var moves: lists.MoveList = .{};
+    move_gen.generate_moves(&b, &moves, types.Color.Black);
+
+    //print("{any}", .{moves});
+
+    for (0..moves.count) |i| {
+        const m = moves.moves[i];
+        // convert the raw u6 back into a square enum
+        const from_sq: types.square = @enumFromInt(m.from);
+        const to_sq: types.square = @enumFromInt(m.to);
+
+        const from_str = types.SquareString.getSquareToString(from_sq);
+        const to_str = types.SquareString.getSquareToString(to_sq);
+        // flags is just a small integer
+
+        try stdout.print("{s}->{s} flags={any}\n", .{ from_str, to_str, m.flags });
+    }
+
     bitboard.print_attacked_squares(&b);
     bitboard.print_attacked_squares_new(&b);
 }
--- src/types.zig
+++ src/types.zig
@@ -12,14 +12,14 @@ pub const number_of_squares = 64;
 // Little endian rank-file (LERF) mapping
 pub const square = enum {
     // zig fmt: off
-    a1, b1, c1, d1, e1, f1, g1, h1,
-    a2, b2, c2, d2, e2, f2, g2, h2,
-    a3, b3, c3, d3, e3, f3, g3, h3,
-    a4, b4, c4, d4, e4, f4, g4, h4,
-    a5, b5, c5, d5, e5, f5, g5, h5,
-    a6, b6, c6, d6, e6, f6, g6, h6,
-    a7, b7, c7, d7, e7, f7, g7, h7,
     a8, b8, c8, d8, e8, f8, g8, h8,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a1, b1, c1, d1, e1, f1, g1, h1, 
     NO_SQUARE,
     // zig fmt: on
     //
@@ -33,18 +33,30 @@ pub const square = enum {
 };
 
 pub const square_number = [_]usize{
-    0,  1,  2,  3,  4,  5,  6,  7,
-    8,  9,  10, 11, 12, 13, 14, 15,
-    16, 17, 18, 19, 20, 21, 22, 23,
-    24, 25, 26, 27, 28, 29, 30, 31,
-    32, 33, 34, 35, 36, 37, 38, 39,
-    40, 41, 42, 43, 44, 45, 46, 47,
-    48, 49, 50, 51, 52, 53, 54, 55,
     56, 57, 58, 59, 60, 61, 62, 63,
+    48, 49, 50, 51, 52, 53, 54, 55,
+    40, 41, 42, 43, 44, 45, 46, 47,
+    32, 33, 34, 35, 36, 37, 38, 39,
+    24, 25, 26, 27, 28, 29, 30, 31,
+    16, 17, 18, 19, 20, 21, 22, 23,
+    8,  9,  10, 11, 12, 13, 14, 15,
+    0,  1,  2,  3,  4,  5,  6,  7,
 };
 
 pub const SquareString = struct {
-    pub const SquareToString = [_][:0]const u8{ "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6", "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8", "None" };
+    pub const SquareToString = [_][:0]const u8{
+        // zig fmt: off
+        "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+        "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+        "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+        "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+        "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+        "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+        "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+        "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+        "None" 
+        // zig fmt: on
+    };
 
     pub fn getSquareToString(sq: square) []const u8 {
         const idx: usize = @intFromEnum(sq);
@@ -65,6 +77,10 @@ pub const Color = enum {
     White,
     Black,
     both,
+
+    pub inline fn toU4(self: Color) u4 {
+        return @as(u4, @truncate(@intFromEnum(self)));
+    }
 };
 
 pub const PieceType = enum(u8) {
@@ -87,7 +103,7 @@ pub const Piece = enum(u8) {
     WHITE_ROOK,
     WHITE_QUEEN,
     WHITE_KING,
-    BLACK_PAWN,
+    BLACK_PAWN = 8,
     BLACK_KNIGHT,
     BLACK_BISHOP,
     BLACK_ROOK,
@@ -102,7 +118,7 @@ pub const Piece = enum(u8) {
 
 pub const unicodePice = &[_][]const u8{
     // zig fmt: off
-    "♟︎", "♞", "♝", "♜", "♛", "♚",
+    "♟︎", "♞", "♝", "♜", "♛", "♚", "~", ">",
     "♙", "♘", "♗", "♖", "♕", "♔", ".",
     // zig fmt: on
 };
@@ -171,7 +187,10 @@ pub const Board = struct {
 
     pub inline fn set_black(self: *Board) Bitboard {
         return self.pieces[Piece.BLACK_PAWN.toU4()] * 8 | self.pieces[Piece.BLACK_KNIGHT.toU4()] | self.pieces[Piece.BLACK_BISHOP.toU4()] | self.pieces[Piece.BLACK_QUEEN.toU4()] | self.pieces[Piece.BLACK_KING.toU4()];
+        self.pieces[Piece.BLACK_PAWN.toU4()] | self.pieces[Piece.BLACK_KNIGHT.toU4()] | self.pieces[Piece.BLACK_BISHOP.toU4()] | self.pieces[Piece.BLACK_QUEEN.toU4()] | self.pieces[Piece.BLACK_KING.toU4()];
+
     }
+
 };
 
 // Attacking directions for the pieces
@@ -328,4 +347,35 @@ pub const squar_bb = [_]u64{
     0x100000000000000,  0x200000000000000,  0x400000000000000,  0x800000000000000,
     0x1000000000000000, 0x2000000000000000, 0x4000000000000000, 0x8000000000000000,
     0x0,
+};
+
+pub const squar_bb_rotated = [_]u64{
+    // zig fmt: off
+    // rank 8
+    0x0100000000000000, 0x0200000000000000, 0x0400000000000000, 0x0800000000000000,
+    0x1000000000000000, 0x2000000000000000, 0x4000000000000000, 0x8000000000000000,
+    // rank 7
+    0x0001000000000000, 0x0002000000000000, 0x0004000000000000, 0x0008000000000000,
+    0x0010000000000000, 0x0020000000000000, 0x0040000000000000, 0x0080000000000000,
+    // rank 6
+    0x0000010000000000, 0x0000020000000000, 0x0000040000000000, 0x0000080000000000,
+    0x0000100000000000, 0x0000200000000000, 0x0000400000000000, 0x0000800000000000,
+    // rank 5
+    0x0000000100000000, 0x0000000200000000, 0x0000000400000000, 0x0000000800000000,
+    0x0000001000000000, 0x0000002000000000, 0x0000004000000000, 0x0000008000000000,
+    // rank 4
+    0x0000000001000000, 0x0000000002000000, 0x0000000004000000, 0x0000000008000000,
+    0x0000000010000000, 0x0000000020000000, 0x0000000040000000, 0x0000000080000000,
+    // rank 3
+    0x0000000000010000, 0x0000000000020000, 0x0000000000040000, 0x0000000000080000,
+    0x0000000000100000, 0x0000000000200000, 0x0000000000400000, 0x0000000000800000,
+    // rank 2
+    0x0000000000000100, 0x0000000000000200, 0x0000000000000400, 0x0000000000000800,
+    0x0000000000001000, 0x0000000000002000, 0x0000000000004000, 0x0000000000008000,
+    // rank 1
+    0x0000000000000001, 0x0000000000000002, 0x0000000000000004, 0x0000000000000008,
+    0x0000000000000010, 0x0000000000000020, 0x0000000000000040, 0x0000000000000080,
+    // none
+    0x0,
+    // zig fmt: on
 };
